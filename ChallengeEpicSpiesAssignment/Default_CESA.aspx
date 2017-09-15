206181788cf838415afd65cbfe471a0e05592cfc
goyaschooling <goyaschooling@gmail.com>
2017-09-14 23:05:18 -0700

    Add All Files

---
+++ ChallengeEpicSpiesAssignment/Default_CESA.aspx
@@ -0,0 +1,55 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default_CESA.aspx.cs" Inherits="ChallengeEpicSpiesAssignment.Default_CESA" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 242px;
+            height: 300px;
+        }
+        .auto-style2 {
+            font-family: Arial, Helvetica, sans-serif;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <img alt="" class="auto-style1" src="epic-spies-logo.jpg" /><br />
+            <h1><span class="auto-style2"><strong>Spy New Assignment Form</strong></span></h1>
+            <br />
+            Spy Code Name:&nbsp;
+            <asp:TextBox ID="nameTextBox" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            New Assignment Name:
+            <asp:TextBox ID="assignNameTextBox" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            End Date of Previous Assignment:
+            <asp:Label ID="selectedPreviousDateLabel" runat="server"></asp:Label>
+&nbsp;<asp:Calendar ID="previousAssignCalendar" runat="server" OnSelectionChanged="previousAssignCalendar_SelectionChanged"></asp:Calendar>
+            <br />
+            <br />
+            Start Date of New Assignment:
+            <asp:Label ID="selectedNewDateLabel" runat="server"></asp:Label>
+            <asp:Calendar ID="newAssignCalendar" runat="server" OnSelectionChanged="newAssignCalendar_SelectionChanged"></asp:Calendar>
+            <br />
+            <br />
+            Projected End Date of New Assignment:
+            <asp:Label ID="selectedNewEndDateLabel" runat="server"></asp:Label>
+            <asp:Calendar ID="newAssignEndCalendar" runat="server" OnSelectionChanged="newAssignEndCalendar_SelectionChanged"></asp:Calendar>
+            <br />
+            <br />
+            <asp:Button ID="assignButton" runat="server" OnClick="assignButton_Click" Text="Assign Spy" />
+            <br />
+            <br />
+            <asp:Label ID="outputLabel" runat="server"></asp:Label>
+            <br />
+        </div>
+    </form>
+</body>
+</html>
---
+++ ChallengeEpicSpiesAssignment/Default_CESA.aspx.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengeEpicSpiesAssignment
+{
+    public partial class Default_CESA : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack) //loads only when first accessing page - initilizing calendar controls
+            {
+                DateTime Today = DateTime.Today;
+                previousAssignCalendar.SelectedDate = Today;
+                newAssignCalendar.SelectedDate = Today.AddDays(+14);
+                newAssignEndCalendar.SelectedDate = Today.AddDays(+21);
+            }
+        }
+        
+        protected void previousAssignCalendar_SelectionChanged(object sender, EventArgs e)
+        {
+            DateTime previousDate = previousAssignCalendar.SelectedDate;
+            //selectedPreviousDateLabel.Text = previousDate.ToShortDateString(); //to display selected date
+        }
+
+
+        protected void newAssignCalendar_SelectionChanged(object sender, EventArgs e)
+        {
+            DateTime newDate = newAssignCalendar.SelectedDate;
+            //selectedNewDateLabel.Text = newDate.ToString(); //to display selected date
+        }
+
+        protected void newAssignEndCalendar_SelectionChanged(object sender, EventArgs e)
+        {
+            DateTime newEndDate = newAssignEndCalendar.SelectedDate;
+            //selectedNewEndDateLabel.Text = newEndDate.ToString(); //to display selected date
+        }
+        
+      
+        protected void assignButton_Click(object sender, EventArgs e)
+        {
+            DateTime Today = DateTime.Today;
+
+            DateTime previousDate = previousAssignCalendar.SelectedDate;
+            selectedPreviousDateLabel.Text = previousDate.ToShortDateString();//to display selected date when button is clicked.
+
+            DateTime newDate = newAssignCalendar.SelectedDate;
+            selectedNewDateLabel.Text = newDate.ToShortDateString();//to display selected date when button is clicked.
+
+            DateTime newEndDate = newAssignEndCalendar.SelectedDate;
+            selectedNewEndDateLabel.Text = newEndDate.ToShortDateString();//to display selected date when button is clicked.
+
+            //String assignmentTimeLength; - DAYS FROM
+
+            TimeSpan betweenAssignments = (previousDate - newDate).Duration(); //14 days between previous date & new date
+            Double daysBetweenAssignments = Convert.ToDouble(betweenAssignments.TotalDays);
+            
+            TimeSpan assignmentStartEnd = (newDate - newEndDate).Duration(); //up to 3 weeks $500 day, more than 3 weeks $500 + $1000
+            Double numberOfAssignmentDays = Convert.ToDouble(assignmentStartEnd.TotalDays);
+
+            /*
+            //String assignmentTimeLength;
+            String assignmentBudgetTotal;
+            //TimeSpan assignmentStartEnd = (previousDate - newDate).Duration();
+            TimeSpan assignmentStartEnd = (previousDate - newDate).Duration();
+            Double numberOfNewAssignmentDays;
+            */
+
+            double assignmentBudgetTotal;
+
+            ///if (numberOfAssignmentDays < 14)
+            if (daysBetweenAssignments < 14)
+                {
+                previousDate.AddDays(+14);
+                newAssignCalendar.SelectedDate = previousDate.AddDays(+14);
+                string selectedNewDateLabel = Convert.ToString(newAssignCalendar.SelectedDate);
+                //string selectedNewDateLabel = Convert.ToString(previousDate.AddDays(+14));
+                //selectedNewDateLabel.Text = Convert.ToString(previousDate.AddDays(+14));
+                string outputLess14days = String.Format("Error: {0}, you must allow at least 2 weeks between " +
+                    "Previous Assignment and New Assignment: (earliest start date of new assignment is:{1:d, M, yy}).", nameTextBox.Text, selectedNewDateLabel);
+                outputLabel.Text = outputLess14days;
+            }
+            //else if ((14 <= numberOfAssignmentDays) && (numberOfAssignmentDays <= 21))//$500/day
+            else if (numberOfAssignmentDays <= 21)//$500/day
+            {
+                double assignmentBudget = (numberOfAssignmentDays * 500);
+                assignmentBudgetTotal = assignmentBudget;
+                string outputResult = String.Format("Assignment of {0} to assignment {1} is authorized. Budget total: {2:C}", nameTextBox.Text, assignNameTextBox.Text, assignmentBudgetTotal);
+                outputLabel.Text = outputResult;
+
+            }
+            else if (21 < numberOfAssignmentDays) //$500/day + $1000
+            {
+                double assignmentBudget = ((numberOfAssignmentDays * 500) + 1000);
+                assignmentBudgetTotal = assignmentBudget;
+                string outputResult = String.Format("Assignment of {0} to assignment {1} is authorized. Budget total: {2:C}", nameTextBox.Text, assignNameTextBox.Text, assignmentBudgetTotal);
+                outputLabel.Text = outputResult;
+            }
+            /*else
+            {
+                assignmentTimeLength = assignmentStartEnd.ToString();
+                //assignmentTimeLength = numberOfAssignmentDays.ToString();
+
+                string outputResult = String.Format("Assignment of {0} to assignment {1} is authorized. Budget total: {2}", nameTextBox.Text, assignNameTextBox.Text, assignmentTimeLength);
+
+                outputLabel.Text = outputResult;
+            */
+        }
+            
+    }
+
+    
+}
